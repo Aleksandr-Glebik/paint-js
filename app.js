7e935218eab35cfe09a68965596814156c9f4534
Aleksandr-Glebik <glebikaa@mail.ru>
2021-12-06 23:44:25 +0300

    feat: getContext canvas, add width amd height to convas; ctx start style; add methodods to canvas

--- app.js
+++ app.js
@@ -1,28 +1,47 @@
 const canvas = document.getElementById('jsCanvas')
+const ctx = canvas.getContext('2d')
+// console.log(ctx);
+
+canvas.height = 700
+canvas.width = 700
+
+ctx.lineWidth = 2.5
+ctx.strokeStyle = '#2c2c2c'
 
 let painting = false
 
 function stopPainting() {
-    let painting = false
+    painting = false
+}
+
+function startPainting() {
+    painting = true
 }
 
 function onMouseMove(event) {
     let x = event.offsetX
     let y = event.offsetY
 
-    console.log(x, y);
+    if (!painting) {
+        ctx.beginPath() // отслеживание стартовой точки
+        ctx.moveTo(x, y) // перемещаем стартовую точку в положение курсора мыши
+    } else {
+        ctx.lineTo(x, y) // видимая линия от стартовой позиции к текущим координатам
+        ctx.stroke() // отрисовываем линию
 
+    }
+///
 }
 
 function onMouseDown(event) {
-    console.log('onMouseDown: ', event);
-    let painting = true
+    // console.log('onMouseDown: ', event);
+    painting = true
 }
 
-function onMouseUp(event) {
+/* function onMouseUp(event) {
     console.log('onMouseUp: ',event);
     stopPainting()
-}
+} */
 
 /* function onMouseLeave() {
     stopPainting()
@@ -31,6 +50,6 @@ function onMouseUp(event) {
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove)
     canvas.addEventListener('mousedown', onMouseDown)
-    canvas.addEventListener('mouseup', onMouseUp)
+    canvas.addEventListener('mouseup', stopPainting)
     canvas.addEventListener('mouseleave', stopPainting)
 }
